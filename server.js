0b1073f77ce5b18cbc67c3f1e521b78f2516677b
yokeTH <sern.dev@gmail.com>
2024-04-13 02:14:37 +0700

    init commit

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.static("public"));
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Frontend Server ready at http://localhost:${PORT}`);
+});
