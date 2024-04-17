3606ede0bd5d9e30b185a60a5d2a259b3eb1e028
yokeTH <sern.dev@gmail.com>
2024-04-17 22:39:44 +0700

    404 not found add

--- server-dev.js
+++ server-dev.js
@@ -13,6 +13,9 @@ liveReloadServer.server.once('connection', () => {
 
 app.use(connectLiveReload());
 app.use(express.static('public'));
+app.use((req, res) => {
+  res.sendFile('./public/404.html', { root: '.' });
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
