cd9d515ea9c70e53777589e12dc57cd4bfe28231
gunkgink <methasith33@gmail.com>
2024-04-19 05:01:59 +0700

    homepage-interface

--- public/controller/main.js
+++ public/controller/main.js
@@ -1,11 +1,8 @@
-import { createUser, login } from 'http://localhost:3000/scripts/api.js';
+import { createUser, login } from 'http://localhost:3000/controller/api.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
   const addUserButton = document.getElementById('register');
   addUserButton.addEventListener('click', () => {
-    // const rname = document.getElementById("regis-username").value;
-    // const remail =document.getElementById("regis-email").value;
-    // const rpass = document.getElementById("regis-password").value;
     createUser(
       document.getElementById('regis-username').value,
       document.getElementById('regis-email').value,
@@ -21,9 +18,6 @@ document.addEventListener('DOMContentLoaded', async () => {
       const name = document.getElementById('login-username').value;
       const pass = document.getElementById('login-password').value;
       const response = await login(name, pass);
-      // localStorage.setItem("accessToken",)
-
-      console.log(response.data);
       localStorage.setItem('token', response.data.accessToken);
       window.location.href = '../homepage/home.html';
     } catch (e) {
