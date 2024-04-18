fba98ee9d7653a92c4c443f3b651f42c986ba521
gunkgink <methasith33@gmail.com>
2024-04-18 19:15:14 +0700

    authentication-finish
    เหลือปรับ uxui

--- public/scripts/api.js
+++ public/scripts/api.js
@@ -1,32 +1,29 @@
-export const backEndUrl = "http://localhost:3001";
+export const backEndUrl = 'http://localhost:3001';
 
-export async function createUser(username,email,password) {
-    await fetch(`${backEndUrl}/user/add-user`, {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify({
-        username:username,
-        email:email,
-        password:password
+export async function createUser(username, email, password) {
+  await fetch(`${backEndUrl}/user/add-user`, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      username: username,
+      email: email,
+      password: password,
+    }),
+  });
+}
 
-      }),
-      
-    });
-  }
-
-export async function login(username,password){
-    await fetch(`${backEndUrl}/user/login`,{
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body :JSON.stringify(
-        {
-          username : username,
-          password : password
-        }
-      )
-    })
+export async function login(username, password) {
+  const info = await fetch(`${backEndUrl}/user/login`, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      username: username,
+      password: password,
+    }),
+  });
+  return info.json();
 }
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,24 +1,33 @@
-import {createUser,login} from "http://localhost:3000/scripts/api.js";
+import { createUser, login } from 'http://localhost:3000/scripts/api.js';
 
+document.addEventListener('DOMContentLoaded', async () => {
+  const addUserButton = document.getElementById('register');
+  addUserButton.addEventListener('click', () => {
+    // const rname = document.getElementById("regis-username").value;
+    // const remail =document.getElementById("regis-email").value;
+    // const rpass = document.getElementById("regis-password").value;
+    createUser(
+      document.getElementById('regis-username').value,
+      document.getElementById('regis-email').value,
+      document.getElementById('regis-password').value,
+    );
+    window.location.href = '../authentication/authen.html';
+  });
 
-document.addEventListener("DOMContentLoaded",() =>{
-    const addUserButton = document.getElementById("register");
-    addUserButton.addEventListener("click", ()=>{
-        // const rname = document.getElementById("regis-username").value;
-        // const remail =document.getElementById("regis-email").value;
-        // const rpass = document.getElementById("regis-password").value;
-        createUser(
-            document.getElementById("regis-username").value,
-            document.getElementById("regis-email").value,
-            document.getElementById("regis-password").value 
-        );
-    });
+  const loginUserButton = document.getElementById('login');
+  loginUserButton.addEventListener('click', async () => {
+    // e.preventDefault
+    try {
+      const name = document.getElementById('login-username').value;
+      const pass = document.getElementById('login-password').value;
+      const response = await login(name, pass);
+      // localStorage.setItem("accessToken",)
 
-    const loginUserButton = document.getElementById("login");
-    loginUserButton.addEventListener("click",()=>{
-        login(
-            document.getElementById("login-username").value,
-            document.getElementById("login-password").value
-        );
-    });
+      console.log(response.data);
+      localStorage.setItem('token', response.data.accessToken);
+      window.location.href = '../homepage/home.html';
+    } catch (e) {
+      console.log('wrong password');
+    }
+  });
 });
